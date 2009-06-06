94d11d62cdc9e5f89a28abf6fc1708460297df09
Petr Uzel <petr.uzel@centrum.cz>
2009-06-06 21:17:27 +0200

    Initial commit
    Works on testing directory
    Many TODOs, rough edges

---
+++ pps.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+#
+# This file is part of the pps project.
+#
+# Copyright (C) 2009 Petr Uzel <petr.uzel@centrum.cz>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+
+
+# check specfile
+spec_file_count=$(ls -1 *.spec 2>/dev/null | wc -l)
+
+if [ $spec_file_count -ne 1 ]; then
+	echo "Error: no specfile found";
+	exit 1;
+fi;
+
+specfile=*.spec
+
+# check for existence of needed utilities
+
+# check if quilt is installed
+if ! which quilt > /dev/null ; then
+	echo "Error: quilt is not installed";
+	exit 1;
+fi;
+
+# check if extracted tarball exists
+#TODO
+
+# quilt setup on specfile
+#TODO quilt returns 0 even if the prep section failed
+#if ! quilt setup $specfile; then
+#	echo "Error: quilt setup failed";
+#	echo 1;
+#fi;
+
+quilt_output=$(mktemp /tmp/pps-qso.XXXXXX)
+
+quilt setup $specfile > $quilt_output 2>&1
+
+#TODO check quilt_output for errors
+
+# get name of the directory with sources
+archives=$(grep '^Unpacking archive' $quilt_output | sed 's/^Unpacking archive \(.*\)/\1/')
+archives_count=$(echo $archives | wc -w)
+
+if [ $archives_count -eq 0 ]; then
+	echo "Error: it seems that no archive was unpacked - strange";
+	exit 1;
+fi;
+
+if [ $archives_count -ge 2 ]; then
+	echo "Error: more than one archive was unpacked - this is not supported (yet)";
+	exit 1;
+fi;
+
+top_dirs_count=$(tar tf $archives | cut -d '/' -f 1 | sort -u | wc -l)
+if [ $top_dirs_count -gt 1 ]; then
+	echo "Error: source tarball contains more than one top-level directory - this is not supported";
+	exit 1;
+fi;
+src_directory=$(tar tf $archives | cut -d '/' -f 1 | sort -u)
+
+# cwd to uncompressed tarball
+cd $src_directory
+
+# quilt push -a
+quilt push -a
+
+# make ctags database
+ctags -R --exclude=.pc .
+
+#TODO: make cscope database
+
+#TODO: set options for osc build (perhaps something like user-defined postscript ?)
+
+
+#TODO accept -h/--help
+#TODO accept -f/--force
+#TODO accept -v/--verbose
+#TODO delete temporary files (with traps?)
+#TODO accept --no-color
+#TODO accept -q/--quiet
